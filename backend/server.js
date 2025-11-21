bb1e5c6b1cc8c4b49a005412ba957b2f544a1dfd
devsujalpatel <sujal7455@gmail.com>
2025-11-21 23:01:21 +0530

    express setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
 import express from "express";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is ok");
+});
+
+app.listen(5001, () => {
+  console.log("Server is running on PORT:5001");
+});
