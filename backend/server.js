73398cf2c464644bf16d363b54b34e3da9b9b2e7
devsujalpatel <sujal7455@gmail.com>
2025-11-23 23:32:56 +0530

    Created post transaction method

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import { sql } from "./config/db.js";
 
 const app = express();
 
+// middleware
+app.use(express.json());
+
 const port = process.env.PORT || 5001;
 
 async function initDB() {
@@ -20,12 +23,29 @@ async function initDB() {
     console.log("Database initialized successfully");
   } catch (error) {
     console.error("Error initializing database:", error);
-    process.exit(1); // status code 1 means failure, 0 success
+    process.exit(1);
   }
 }
 
-app.get("/", (req, res) => {
-  res.send("Server is running ok");
+app.post("/api/transactions", async (req, res) => {
+  try {
+    const { title, amount, category, user_id } = req.body;
+
+    if (!title || amount === undefined || !category || !user_id) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const transaction = await sql`
+    INSERT INTO transactions(user_id, title, amount, category)
+    VALUES (${user_id}, ${title}, ${amount}, ${category})
+    RETURNING *
+    `;
+
+    res.status(201).json(transaction[0])
+  } catch (error) {
+    console.error("Error creating transaction:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 initDB().then(() => {
