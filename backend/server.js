c79b6f8d5922b063104f93ee9571a3028f961a58
devsujalpatel <sujal7455@gmail.com>
2025-11-23 23:51:20 +0530

    Deleting transactions by id created

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,27 @@ app.post("/api/transactions", async (req, res) => {
   }
 });
 
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await sql`
+    DELETE FROM transactions WHERE id = ${id} RETURNING * 
+    `;
+
+    if (result.length === 0) {
+      return res.status(404).json({
+        message: "Transaction not found",
+      });
+    }
+
+    res.status(200).json({ message: "Transaction deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting transaction:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 initDB().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
