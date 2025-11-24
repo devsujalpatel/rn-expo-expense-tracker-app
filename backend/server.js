95668576b34a31a7ffd23fb070ac3a4e929dced8
devsujalpatel <sujal7455@gmail.com>
2025-11-25 04:42:23 +0530

    code refactor

--- backend/server.js
+++ backend/server.js
@@ -93,17 +93,17 @@ app.get("/api/transactions/sumary/:userId", async (req, res) => {
     const { userId } = req.params;
 
     const balanceResult = await sql`
-    SELECT COALESCE(SUM(amount),0) as balance FROM transactions
-     WHERE user_id = ${userId}
+      SELECT COALESCE(SUM(amount),0) as balance FROM transactions
+      WHERE user_id = ${userId}
     `;
 
     const incomeResult = await sql`
-  SELECT COALESCE(SUM(amount),0) as income FROM transactions 
-  WHERE user_id = ${userId} AND amount > 0  
+      SELECT COALESCE(SUM(amount),0) as income FROM transactions 
+      WHERE user_id = ${userId} AND amount > 0  
   `;
     const expensesResult = await sql`
-  SELECT COALESCE(SUM(amount),0) as expenses FROM transactions 
-  WHERE user_id = ${userId} AND amount < 0  
+      SELECT COALESCE(SUM(amount),0) as expenses FROM transactions 
+      WHERE user_id = ${userId} AND amount < 0  
   `;
 
     res.status(200).json({
