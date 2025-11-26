16a411b0195284f7805422f7eecbf13e888d9a98
devsujalpatel <sujal7455@gmail.com>
2025-11-26 22:44:21 +0530

    Code refactor

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 import { Ratelimit } from "@upstash/ratelimit";
-import "@dotenv/config";
+import "dotenv/config";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,19 @@
 import express from "express";
 import "dotenv/config";
-import { sql } from "./config/db.js";
-import transactionRouter from "./routes/trasactions.routes.js";
+import transactionRoutes from "./routes/trasactions.routes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import { initDB } from "./config/initDb.js";
 
 const app = express();
-
-// middleware
-app.use(express.json());
-app.use(rateLimiter);
-
 const port = process.env.PORT || 5001;
 
-async function initDB() {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions(
-      id SERIAL PRIMARY KEY,
-      user_id VARCHAR(255) NOT NULL,
-      title VARCHAR(255) NOT NULL,
-      amount DECIMAL(10,2) NOT NULL,
-      category VARCHAR(255) NOT NULL,
-      created_at DATE NOT NULL DEFAULT CURRENT_DATE
-    )`;
+// middlewares
+app.use(express.json());
+app.use(rateLimiter); // ratelimit middleware
 
-    console.log("Database initialized successfully");
-  } catch (error) {
-    console.error("Error initializing database:", error);
-    process.exit(1);
-  }
-}
 
-app.use("/api/transactions", transactionRouter);
+// routes
+app.use("/api/transactions", transactionRoutes);
 
 initDB().then(() => {
   app.listen(port, () => {
