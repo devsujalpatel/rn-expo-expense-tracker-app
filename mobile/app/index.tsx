fa4ad740e90b25bfc47bf21485925b68fa698e1e
devsujalpatel <sujal7455@gmail.com>
2025-12-03 23:10:43 +0530

    Image comp

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,3 +1,4 @@
+import { Image } from "expo-image";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
@@ -12,11 +13,19 @@ export default function Index() {
       }}
     >
       <Text
-      style={{
-        color: "blue"
-      }}
-      >Edit app/index.tsx to edit this screen.</Text>
+        style={{
+          color: "blue",
+        }}
+      >
+        Edit app/index.tsx to edit this screen.
+      </Text>
       <Link href="/about">About</Link>
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1764703495149-f09b0aa607c3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
 }
