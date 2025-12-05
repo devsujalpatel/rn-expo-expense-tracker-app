e72002e1d7f7b23dd191d810ecc6b43427ac56c0
devsujalpatel <sujal7455@gmail.com>
2025-12-05 22:24:37 +0530

    Stylesheet in react-native

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,39 +1,16 @@
 import { Image } from "expo-image";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#E6F4FE",
-      }}
-    >
-      <Text
-        style={{
-          color: "blue",
-        }}
-      >
-        Edit app/index.tsx to edit this screen.
-      </Text>
-      <Link href="/about">About</Link>
-      {/* <Image
-        source={{
-          uri: "https://images.unsplash.com/photo-1764703495149-f09b0aa607c3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
-        }}
-        style={{ width: 200, height: 200 }}
-      /> */}
-      <Image
-        source={require("@/assets/images/icon.png")}
-        style={{ width: 200, height: 200 }}
-      />
-
-      <View>
-        <Text>Hello This is using</Text>
-      </View>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#E6F4FE",
+  },
+});
