478d50dadf70de39dc5cd7a7cbba976e6b63782c
devsujalpatel <sujal7455@gmail.com>
2025-12-06 22:49:43 +0530

    Safe area view comp

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,20 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Hello World!</Text>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#E6F4FE",
   },
-  header: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
+
 });
