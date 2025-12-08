da72ad439a4171167d8e01893fed2a66f5d702cb
devsujalpatel <sujal7455@gmail.com>
2025-12-08 23:30:05 +0530

    working

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,15 +1,16 @@
+import { Slot } from "expo-router";
 import SafeScreen from "@/components/SafeScreen";
 import { ClerkProvider } from "@clerk/clerk-expo";
-import { Slot } from "expo-router";
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { StatusBar } from "expo-status-bar";
 
-
-function RootLayout() {
+export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <SafeScreen>
         <Slot />
       </SafeScreen>
+      <StatusBar style="dark" />
     </ClerkProvider>
   );
 }
